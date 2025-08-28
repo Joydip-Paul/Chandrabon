cad3d31fed129712a68f675f5fdbda9dc10a338c
joydip-paul <joydippaul72@gmail.com>
2025-08-29 00:53:22 +0600

    chandrabon hero

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config & any = {
+  content: ["./src/**/*.{ts,tsx,js,jsx,html}", "./app/**/*.{ts,tsx,js,jsx,html}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        border: "var(--border)",
+        txtColor: "var(--txtColor)",
+        txtColorSecondary: "var(--txtColorSecondary)",
+      },
+      boxShadow: {
+        "custom-shadow": "rgba(149, 157, 165, 0.2) 0px 8px 24px",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
